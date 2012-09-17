5296ece3ae5e6990eba856848902ff6d263501a3
crybert <christian.berten@gmx.de>
2012-09-17 11:15:38 +0200

    removed NovaLauncher

--- configs/common_phone.mk
+++ configs/common_phone.mk
@@ -1,9 +1,5 @@
 $(call inherit-product, vendor/aokp/configs/common.mk)
 
-PRODUCT_COPY_FILES += \
-    vendor/aokp/prebuilt/common/app/NovaLauncher.apk:system/app/NovaLauncher.apk
-
-
 # Inherit drm blobs
 -include vendor/aokp/configs/common_drm_phone.mk
 
