0b290ffa3fa8401a60a0afb9aafceab848d88b50
crybert <christian.berten@gmx.de>
2012-09-20 16:16:44 +0200

    changed some build.prop strings

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -1,14 +1,15 @@
 # Version information used on all builds
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=IMM76I BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_UTC_DATE=$(shell date +"%s")
 
+DATE = $(shell date +%Y%m%d-%H%M)
+
 # Rom Manager properties
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.rommanager.developerid=teamkang \
-    ro.aokp.version=$(TARGET_PRODUCT)_milestone-6
+    ro.aokp.version=$(TARGET_PRODUCT)_milestone-6_build_3
 
-# Goo updater app
+# OTAUpdater
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.goo.developerid=aokp \
-    ro.goo.rom=aokp \
-    ro.goo.version=19
+    otaupdater.otaid=romidaokpcrybert \
+    otaupdater.otatime=$(DATE) \
+    otaupdater.otaver=build 3
 
